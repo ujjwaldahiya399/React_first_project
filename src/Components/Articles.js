ada27d942803d42c6f37cf2ec54979dcef4a0d6f
Ujjwal dahiya <ujjwaldahiya350@gmail.com>
2023-09-20 11:46:35 +0530

    deleting unused vars

--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./Articles.css";
-// import ".App.css";
-// import { LinearProgress } from "@muimaterial";
+
 import CircularProgress from '@mui/material/CircularProgress';
 import { useParams,Link } from "react-router-dom";
 import axios from "axios";
